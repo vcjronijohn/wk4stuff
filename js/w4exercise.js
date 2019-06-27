246677a8b34b9317dbb3686dbebc62900de1a9fe
Veronica Johnson <ronijohnveronica@gmail.com>
2019-06-27 15:48:08 -0400

    second commit

--- js/w4exercise.js
+++ js/w4exercise.js
@@ -12,3 +12,17 @@ function addClass() {
 	text.classList.remove('hidden');
 	text.classList.add('show');
 }
+//https://www.includehelp.com/code-snippets/dynamically-add-css-class-with-javascript.aspx
+
+// var mi = document.createElement("input");
+// mi.type= "text"
+// mi.value = "default"
+
+// var mi = document.createElement("input");
+// mi.setAttribute('type', 'text');
+// mi.setAttribute('value', 'default');
+
+// var i = document.createElement("input"); //input element, text
+// i.setAttribute('type',"text");
+// i.setAttribute('name',"username");
+// i.setAttribute('value',"default");
